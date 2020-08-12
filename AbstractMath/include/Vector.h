89ae2bb355c25472b934440766fd05b9064264da
Eddie Ibarra <39746205+eddieibarra4242@users.noreply.github.com>
2020-08-11 21:15:26 -0700

    Vector Base Class

--- AbstractMath/include/Vector.h
+++ AbstractMath/include/Vector.h
@@ -2,15 +2,136 @@
 
 #include <type_traits>
 #include <memory>
+#include <assert.h>
 
-template<typename T, size_t TCount>
-class Vector {
+template<typename T, size_t C>
+class Vector 
+{
+public:
 	static_assert(std::is_arithmetic<T>::value, "Type must be number");
 
-	Vector() {
-		data = std::make_unique(sizeof(T) * TCount);
+	Vector() 
+	{
+		data = new T[C];
 	}
 
+	~Vector()
+	{
+		delete[] data;
+	}
+
+	T& operator[](size_t index) const
+	{
+		assert(index < C, "Index out of bounds");
+		return data[index];
+	}
+
+	template<typename Ty, size_t Cy>
+	Vector<decltype(T(0) + Ty(0)), C> operator+(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		Vector<decltype(T(0) + Ty(0)), C> result;
+
+		for (size_t i = 0; i < C; i++)
+		{
+			result[i] = (*this)[i] + other[i];
+		}
+
+		return result;
+	}
+
+	template<typename Ty, size_t Cy>
+	Vector<decltype(T(0) - Ty(0)), C> operator-(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		Vector<decltype(T(0) - Ty(0)), C> result;
+
+		for (size_t i = 0; i < C; i++)
+		{
+			result[i] = (*this)[i] - other[i];
+		}
+
+		return result;
+	}
+
+	template<typename Ty, size_t Cy>
+	Vector<decltype(T(1) * Ty(1)), C> operator*(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		Vector<decltype(T(1) * Ty(1)), C> result;
+
+		for (size_t i = 0; i < C; i++)
+		{
+			result[i] = (*this)[i] * other[i];
+		}
+
+		return result;
+	}
+
+	template<typename Ty, size_t Cy>
+	Vector<decltype(T(1) / Ty(1)), C> operator/(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		Vector<decltype(T(1) / Ty(1)), C> result;
+
+		for (size_t i = 0; i < C; i++)
+		{
+			result[i] = (*this)[i] / other[i];
+		}
+
+		return result;
+	}
+
+	template<typename Ty, size_t Cy>
+	void operator+=(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		for (size_t i = 0; i < C; i++)
+		{
+			(*this)[i] += other[i];
+		}
+	}
+
+	template<typename Ty, size_t Cy>
+	void operator-=(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		for (size_t i = 0; i < C; i++)
+		{
+			(*this)[i] -= other[i];
+		}
+	}
+
+	template<typename Ty, size_t Cy>
+	void operator*=(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		for (size_t i = 0; i < C; i++)
+		{
+			(*this)[i] *= other[i];
+		}
+	}
+
+	template<typename Ty, size_t Cy>
+	void operator/=(const Vector<Ty, Cy>& other) const
+	{
+		static_assert(C == Cy, "Vectors must be same size");
+
+		for (size_t i = 0; i < C; i++)
+		{
+			(*this)[i] /= other[i];
+		}
+	}
+
+	//implement copies and moves
+
 private:
-	std::unique_ptr<T> data;
+	T* data;
 };
